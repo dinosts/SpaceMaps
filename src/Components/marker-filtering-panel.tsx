1b0cc5d55155a0ee4407973a817966b6a870a2fb
konstantinos <konstantinos.tsitoumis@splyt.com>
2022-10-15 16:17:02 +0300

    drawer

--- src/Components/marker-filtering-panel.tsx
+++ src/Components/marker-filtering-panel.tsx
@@ -3,14 +3,14 @@ import {Box, FormControl, InputLabel, MenuItem, Select} from "@mui/material";
 function MarkerFilteringPanel() {
     return (
         <div style={{
-            position: 'absolute',
-            height: '45px',
+            position: 'fixed',
+            height: '40px',
             padding:'10px',
             top:'80px',
             background:'white',
-            borderRadius:'10%',
+            borderRadius:'5%',
             borderColor:'black',
-            width:'100px',
+            width:'90px',
         }}>
             <FormControl size='small' fullWidth>
                 <InputLabel >Type</InputLabel>
--- src/Components/space-menu.tsx
+++ src/Components/space-menu.tsx
@@ -1,18 +1,14 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 
 import {
     AppBar, Avatar, Box,
-    Button, Grid,
+    Button, Grid, SwipeableDrawer,
     Toolbar,
     Typography,
 } from "@mui/material";
 
-// @ts-ignore
-import logo from '../logo.svg';
+import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
 
-import {
-    ArrowBack
-} from '@mui/icons-material'
 
 interface PinData {
     lat: number;
@@ -26,15 +22,26 @@ interface PinData {
 
 function SpaceMenu() {
 
+    const [needsDrawer, setNeedsDrawer] = React.useState<boolean>(false)
+
+    useEffect(()=>{
+        if (window.innerWidth < 756) {
+            setNeedsDrawer(true)
+        }
+        else {
+            setNeedsDrawer(false);
+        }
+    },[setNeedsDrawer])
+
     // breaks at width 756
     //
     return (
-        <AppBar >
-            <Toolbar sx={{ background: 'white', color:'#54a5f7'}}>
+         <AppBar >
+            <Toolbar sx={{ background: 'white', color:'#54a5f7', hidden:'true'}}>
                 <Grid container spacing={3} justifyContent="space-between" alignItems="center" sx={{marginTop:'0px !important'}}>
                     <Grid sm={3} sx={{textAlign:'start'}}>
                         <Button>
-                            <ArrowBack /> Back to HomePage
+                            <ArrowBackIosIcon /> Back to HomePage
                         </Button>
                     </Grid>
                     <Grid sm={6} sx={{display: 'contents', textAlign:'center'}} >
@@ -55,9 +62,79 @@ function SpaceMenu() {
                 </Grid>
 
             </Toolbar>
-        </AppBar>
+             <SpaceDrawer/>
+         </AppBar>
+
     );
 }
 
 
 export default SpaceMenu;
+
+function SpaceDrawer(){
+    const [state, setState] = React.useState<boolean>(false)
+
+    const toggleDrawer =
+        ( open: boolean) =>
+            (event: React.KeyboardEvent | React.MouseEvent) => {
+                if (
+                    event &&
+                    event.type === 'keydown' &&
+                    ((event as React.KeyboardEvent).key === 'Tab' ||
+                        (event as React.KeyboardEvent).key === 'Shift')
+                ) {
+                    return;
+                }
+
+                setState(open);
+            };
+
+    return (<React.Fragment key={'right'}>
+        <Button onClick={toggleDrawer(true)}
+                sx={{
+                    position:'fixed',
+                    right:'-25px',
+                    top:'50%',
+                    background:'white',
+                    borderRadius:'100%',
+                    width:'55px',
+                    height:'55px',
+                }}>
+            <ArrowBackIosIcon/>
+        </Button>
+        <SwipeableDrawer
+            anchor={'right'}
+            open={state}
+            onClose={toggleDrawer(false)}
+            onOpen={toggleDrawer( true)}
+        >
+            <Grid
+                container
+                direction="column"
+                justifyContent="space-between"
+                alignItems="center"
+                sx={{ width:'300px'}}>
+                <Grid>
+                    <Typography
+                        variant="h6"
+                        component="div"
+                    >
+                        SpaceTime Maps 🚩
+                    </Typography>
+                </Grid>
+                <Grid  sx={{display: 'contents'}} >
+                    <Button sx={{background:'#e8eedc'}}>
+                        <Avatar src={'https://lh6.googleusercontent.com/-UYKv4Oo4AL4/AAAAAAAAAAI/AAAAAAAAAAA/ACHi3reOcBoiruxfuiRHs8VAoE-HvgnBDw/mo/photo.jpg?sz=256'}/>
+                        <Typography >Konstantinos</Typography>
+                    </Button>
+                </Grid>
+
+                <Grid>
+                    <Button>
+                        <ArrowBackIosIcon /> Back to HomePage
+                    </Button>
+                </Grid>
+            </Grid>
+        </SwipeableDrawer>
+    </React.Fragment>);
+}
